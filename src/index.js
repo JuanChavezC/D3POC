a13be7f0677be9838a175ea630d346b589b02377
Juan Chavez <juan.chavez@jalasoft.com>
2017-09-25 18:56:27 -0400

    Modified bar chart using C3.

--- src/index.js
+++ src/index.js
@@ -1,44 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './Header';
-import C3Chart from 'c3-react';
+import C3Chart from 'react-c3js';
 import 'c3/c3.css';
 
-let data = [
-    {
-        key: "Suggestions by week",
-        values: [
-            {label: "May 28", value: 349},
-            {label: "Jun 4", value: 667},
-            {label: "Jun 11", value: 611},
-            {label: "Jun 18", value: 292},
-            {label: "Jun 25", value: 403},
-            {label: "Jul 2", value: 355},
-            {label: "Jul 9", value: 673},
-            {label: "Jul 16", value: 616},
-            {label: "Jul 23", value: 298},
-            {label: "Jul 30", value: 412},
-            {label: "Aug 7", value: 351},
-            {label: "Aug 14", value: 302}
-        ]
+const data = {
+    x : 'x',
+    columns: [
+        ['x', 'May 28', 'Jun 4', 'Jun 11', 'Jun 18', 'Jun 25', 'Jul 2', 'Jul 9', 'Jul 16', 'Jul 23', 'Jul 30', 'Aug 7', 'Aug 14'],
+        ['pv', 349, 667, 611, 292, 403, 355, 673, 616, 298, 412, 351, 302],
+    ],
+    type: 'bar',
+    order: null
+};
+const color = {pattern: ['#1D9283']};
+
+const bar = {
+    width: {
+        ratio: 0.7
     }
-];
+};
 
-let type = "bar";
-
-let options = {
-    axisLabel: {
-        x: {
-            show: false
-        },
-        y: {
-            show: false
-        }
+const axis = {
+    x: {
+        type: 'category',
+        height: 130
     },
-    legend: false
+    y: {
+        show: false
+    }
+};
+
+const legend = {
+    show: false
 };
 
 ReactDOM.render(<Header />, document.getElementById('header'));
-ReactDOM.render(<C3Chart data={data} type={type} options={options}/>, document.getElementById('chart'));
+ReactDOM.render(<C3Chart data={data} axis={axis} color={color} bar={bar} legend={legend} />, document.getElementById('chart'));
 
 
